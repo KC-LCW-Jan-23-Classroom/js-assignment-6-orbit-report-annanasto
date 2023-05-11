99865994e0ded5564c7de8f1be746d3545110f1c
annanasto <anna.nasto18@gmail.com>
2023-05-10 22:58:48 -0500

    fixed satellite count

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -19,7 +19,7 @@ export class OrbitCountsComponent implements OnInit {
 	let count = 0;
 	if (this.satellites) {
 	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
+		 if (this.satellites[i].type.toLowerCase() === type) {
 			count++;
 		 }
 	  }
